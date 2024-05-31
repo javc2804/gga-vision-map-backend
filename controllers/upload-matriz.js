6e11a95be77c3f16a1cc66908a1fde6b2d3f54bd
javier <javc2804@gmail.com>
2024-05-30 21:04:31 -0400

    insercion de matriz por primera vez

--- controllers/upload-matriz.js
+++ controllers/upload-matriz.js
@@ -2,11 +2,10 @@ import multer from "multer";
 import XLSX from "xlsx";
 import Transaction from "../models/transaction.js"; // Import Transaction model
 
-// Configure multer
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const uploadMatriz = multer({ storage: storage });
 
-const uploadExcel = async (req, res) => {
+const uploadExcelMatriz = async (req, res) => {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
@@ -15,40 +14,48 @@ const uploadExcel = async (req, res) => {
     const workbook = XLSX.read(req.file.buffer, { type: "buffer" });
     const sheetName = workbook.SheetNames[1]; // Use the index of the sheet you want to read
     const worksheet = workbook.Sheets[sheetName];
-    const rows = XLSX.utils.sheet_to_json(worksheet, { header: "A" });
+    const allRows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+    const rows = allRows.slice(16); // Omit the first 16 rows
 
-    // Create an object to track unique rows
-    const uniqueTransactions = {}; // Renamed to uniqueTransactions
+    const uniqueTransactions = {};
+    const transactions = [];
 
-    rows.forEach((row) => {
-      // Ignore rows with null 'ut'
-      if (row.A === null) {
-        return;
-      }
-
+    rows.forEach((row, index) => {
       const transaction = {
-        // Renamed to transaction
-        status: row.E, // E2
-        attention: row.I, // I2
-        ut: row.A, // A2
-        eje: row.B, // B1
-        subeje: row.C, // C1
-        marcaModelo: row.D, // D1
-        // Add other fields as necessary
+        ut: row[1],
+        eje: row[3],
+        subeje: row[4],
+        proveedor: row[5] || "",
+        marcaModelo: row[2],
+        descripcion: row[6], // Descripción
+        repuesto: row[6] || "repu", // Descripción
+        descripcionRepuesto: row[6] || "repu", // Descripción
+        cantidad: row[7] || 0, // Cantidad
+        precioUnitarioBs: row[8] || 0, // Precio unitario en bs
+        montoTotalBs: row[9] || 0, // Monto total en Bs
+        precioUnitarioUsd: row[10] || 0, // Precio unitario en Usd
+        montoTotalUsd: row[11] || 0, // Monto total en Usd
+        deudaTotalUsd: row[13] || 0, // Deuda en Usd
+        status: row[7],
+        formaPago: "defa",
+        numeroOrdenPago: row[57] || 0,
+        fechaOcOs: isValidDate(row[55]) ? new Date(row[55]) : new Date(),
+        tasaBcv: row[54] || 0,
+        observacion: row[58] || "",
+        ocOs: "",
+        user_rel: "javier@admin.com",
       };
 
-      // Use 'ut' as key to ensure no duplicates
-      uniqueTransactions[transaction.ut] = transaction; // Renamed to uniqueTransactions
+      uniqueTransactions[index] = transaction;
+      transactions.push(transaction);
     });
 
-    // Convert the values of the object to an array
-    const transactions = Object.values(uniqueTransactions); // Renamed to transactions
-
-    const result = await Transaction.bulkCreate(transactions, {
-      // Changed to Transaction.bulkCreate
-      updateOnDuplicate: ["status", "attention", "ut"],
-    });
-    console.log(`Inserted ${result.length} rows`);
+    try {
+      const result = await Transaction.bulkCreate(transactions);
+      console.log(`Inserted ${result.length} rows`);
+    } catch (error) {
+      console.error("Error during insertion:", error);
+    }
     res.status(200).send("File uploaded and data imported successfully.");
   } catch (error) {
     console.error("Error importing data: ", error);
@@ -56,4 +63,9 @@ const uploadExcel = async (req, res) => {
   }
 };
 
-export { upload, uploadExcel };
+function isValidDate(dateString) {
+  const date = new Date(dateString);
+  return !isNaN(date);
+}
+
+export { uploadMatriz, uploadExcelMatriz };
--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { upload, uploadExcel } from "../controllers/upload.js";
-// import { upload, uploadExcel } from "../controllers/upload-matriz.js";
+import {
+  uploadMatriz,
+  uploadExcelMatriz,
+} from "../controllers/upload-matriz.js";
 
 const router = express.Router();
 
 router.post("/", upload.single("file"), uploadExcel);
-// router.post("/matriz", upload.single("file"), uploadExcel);
+router.post("/matriz", uploadMatriz.single("file"), uploadExcelMatriz);
 
 export default router;
