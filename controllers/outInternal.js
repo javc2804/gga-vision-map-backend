cc32ca68826146be46d2e47894a19df7c405be21
javier <javc2804@gmail.com>
2024-06-26 14:08:58 -0400

    agregado de varios registros de funcionamiento

--- controllers/outInternal.js
+++ controllers/outInternal.js
@@ -2,29 +2,51 @@ import OutInternal from "../models/outInternal.js"; // Asegúrate de que la ruta
 
 const register = async (req, res) => {
   try {
-    // Clonar el objeto data para no modificar el original
-    let data = { ...req.body.data };
+    const preparedData = req.body.data.map((data) => {
+      if (
+        data.fecha_tasa &&
+        !isNaN(new Date(data.fecha_tasa + "T00:00:00").getTime())
+      ) {
+        data.fecha_tasa = new Date(data.fecha_tasa + "T00:00:00");
+      } else {
+        data.fecha_tasa = null; // O manejar de otra manera
+      }
 
-    if (data.fecha_tasa) {
-      data.fecha_tasa = new Date(data.fecha_tasa + "T00:00:00");
-    }
-    if (data.fecha_factura) {
-      data.fecha_factura = new Date(data.fecha_factura + "T00:00:00");
-    }
+      // Repetir para fecha_factura y fecha_pago con la misma lógica
+      if (
+        data.fecha_factura &&
+        !isNaN(new Date(data.fecha_factura + "T00:00:00").getTime())
+      ) {
+        data.fecha_factura = new Date(data.fecha_factura + "T00:00:00");
+      } else {
+        data.fecha_factura = null; // O manejar de otra manera
+      }
 
-    if (data.fecha_pago) {
-      data.fecha_pago = new Date(data.fecha_pago + "T00:00:00");
-    }
+      if (
+        data.fecha_pago &&
+        !isNaN(new Date(data.fecha_pago + "T00:00:00").getTime())
+      ) {
+        data.fecha_pago = new Date(data.fecha_pago + "T00:00:00");
+      } else {
+        data.fecha_pago = null; // O manejar de otra manera
+      }
 
-    const newOutInternal = await OutInternal.create(data);
+      return data;
+    });
+
+    console.log(preparedData);
+
+    const results = await OutInternal.bulkCreate(preparedData, {
+      validate: true,
+    });
 
     res.status(201).json({
-      message: "Registro creado con éxito",
-      data: newOutInternal,
+      message: "Registros creados con éxito",
+      data: results,
     });
   } catch (error) {
     res.status(500).json({
-      message: "Error al crear el registro",
+      message: "Error al crear los registros",
       error: error.message,
     });
   }
