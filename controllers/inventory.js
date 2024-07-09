3039b84713a4f463acb24ca40405bd7f5f21100e
javier <javc2804@gmail.com>
2024-07-09 14:02:16 -0400

    nuevo endpoint para consultar el inventario de una descropcion

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -9,7 +9,27 @@ const getInventory = async (req, res) => {
     res.status(500).json({ error: "Error al obtener el inventario" });
   }
 };
+const getInventoryByDescription = async (req, res) => {
+  try {
+    const { description } = req.body; // Obtiene el parámetro de consulta 'description'
+    if (!description) {
+      return res.status(400).json({ error: "La descripción es requerida" });
+    }
+    const inventoryItems = await Inventory.findAll({
+      where: {
+        descripcion: description, // Asume que 'descripcion' es el nombre de la columna en tu tabla
+      },
+    });
+    res.status(200).json(inventoryItems);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "Error al obtener el inventario por descripción" });
+  }
+};
 
 export default {
   getInventory,
+  getInventoryByDescription,
 };
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import inventary from "../controllers/inventory.js";
 
-const { getInventory } = inventary;
+const { getInventory, getInventoryByDescription } = inventary;
 
 const router = express.Router();
 
 router.get("/list", getInventory);
+router.post("/description", getInventoryByDescription);
 
 export default router;
