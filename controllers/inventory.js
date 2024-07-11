10f0ad14c695f56663dcab4feaa43ebb983d99c4
javier <javc2804@gmail.com>
2024-07-11 05:18:47 -0400

    funcion que agrega al inventario

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -29,7 +29,38 @@ const getInventoryByDescription = async (req, res) => {
   }
 };
 
+const updateInventoryQuantity = async (req, res) => {
+  try {
+    // Extraer el objeto description del cuerpo de la solicitud
+    const { description } = req.body;
+    const { id, name: additionalQuantity } = description;
+
+    // Buscar el registro de inventario por ID
+    const inventoryItem = await Inventory.findByPk(id);
+    if (!inventoryItem) {
+      return res
+        .status(404)
+        .json({ error: "Registro de inventario no encontrado" });
+    }
+
+    // Convertir name a número y sumarlo a la cantidad actual
+    const newQuantity = inventoryItem.cantidad + Number(additionalQuantity);
+
+    // Actualizar la cantidad en el registro
+    await inventoryItem.update({ cantidad: newQuantity });
+
+    res
+      .status(200)
+      .json({ message: "Cantidad actualizada con éxito", inventoryItem });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "Error al actualizar la cantidad del inventario" });
+  }
+};
 export default {
   getInventory,
   getInventoryByDescription,
+  updateInventoryQuantity,
 };
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import inventary from "../controllers/inventory.js";
 
-const { getInventory, getInventoryByDescription } = inventary;
+const { getInventory, getInventoryByDescription, updateInventoryQuantity } =
+  inventary;
 
 const router = express.Router();
 
 router.get("/list", getInventory);
 router.post("/description", getInventoryByDescription);
+router.post("/add", updateInventoryQuantity);
 
 export default router;
